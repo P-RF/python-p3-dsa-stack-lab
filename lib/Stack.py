34232bb48959d4be64ce8436ba4866f78477994b
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-08-16 18:12:03 -0400

    complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,38 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        if items is None:
+            self.items = []
+        else:
+            self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if not self.full():
+            self.items.append(item)
 
     def pop(self):
-        pass
+        if self.items: # if the stack is not empty, pop item
+            return self.items.pop()
+        else:
+            return None # return None if stack is empty
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        distance = 0
+        for item in reversed(self.items):
+            if item == target:
+                return distance
+            distance += 1
+        return -1
